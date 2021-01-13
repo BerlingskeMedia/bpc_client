db98693c9d40e15b499f104f46a79c2272a360bc
Piotr Kurek <xpiku@berlingskemedia.dk>
2021-01-13 13:34:41 +0100

    Fix property to be optional

--- lib/index.ts
+++ lib/index.ts
@@ -29,7 +29,7 @@ export interface BpcRequestOptions extends RequestOptions {
 
 export interface BpcClient {
   events: EventEmitter;
-  app: AppTicket | null;
+  app: AppTicket;
   url: string;
   appTicket: AppTicket | null;
   ticketBuffer: number;
@@ -171,7 +171,7 @@ const client: BpcClient = {
     }
   },
 
-  connect: async (app: AppTicket, url?: string): Promise<void> => {
+  connect: async (app: AppTicket = client.app, url?: string): Promise<void> => {
     const newApp = {
       ...module.exports.app,
       ...app,
